1a8ccd596ceb841de7d96bd5c86e3747da02fcf5
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-12-22 22:23:57 -0300

    Adicionado componente para textos com mais informações pessoais

---
+++ src/components/MyInformation.vue
@@ -0,0 +1,23 @@
+<template>
+  <InfoTable />
+</template>
+
+<script>
+import InfoTable from './InfoTable.vue'
+
+export default {
+  name: 'MyInformation',
+  components: {
+    InfoTable
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+a {
+  color: #42b983;
+}
+</style>

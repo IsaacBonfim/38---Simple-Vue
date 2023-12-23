b3d614008876defc6e069933d5318cd8a46d7f21
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-12-22 22:19:50 -0300

    Criado um novo header para utilização na página

---
+++ src/components/VueHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="Vue-Header">
+    <h1 class="title">Bem vindo minha página simples de Vue</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'VueHeader'
+}
+</script>
+
+<style>
+ .Vue-Header {
+   align-items: center;
+   display: flex;
+   height: 7vh;
+   justify-content: center;
+   margin-bottom: 7px;
+   width: 100vw;
+ }
+
+ .title {
+  color: white;
+  text-shadow: -1px 1px 3px paleturquoise,
+    -2px 2px 6px turquoise;
+ }
+
+</style>

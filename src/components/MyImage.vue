96c916bc354d7d7fa969a23113a21ed4ab0603b8
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-12-22 22:23:04 -0300

    Criado componente para utilização de imagens

---
+++ src/components/MyImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="image" alt="Vue logo" src="../assets/Eu.jpeg" />
+</template>
+
+<script>
+export default {
+  name: 'MyImage'
+}
+</script>
+
+<style>
+  .image {
+    border: 1px solid rgba(64, 224, 208, 0.2);
+    border-radius: 8px;
+    box-shadow: 0 4px 1px 1px transparent,
+      0 4px 20px 4px rgba(64, 224, 208, 0.2);
+    height: 85%;
+    width: 70%;
+  }
+</style>

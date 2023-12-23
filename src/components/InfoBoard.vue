8632b7da77a76136142edb23214bc75461b26556
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-12-22 22:21:07 -0300

    Criado o componente 'InfoBoard' onde ficaram algumas das principais informações da página

---
+++ src/components/InfoBoard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="board glass">
+    <section class="area-1">
+      <MyImage />
+    </section>
+    <section class="area-2">
+      <InfoTable />
+    </section>
+    <section class="area-3">
+      <h1>Oi</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+import InfoTable from './InfoTable.vue'
+import MyImage from './MyImage.vue'
+
+export default {
+  name: 'InfoBoard',
+  components: {
+    InfoTable,
+    MyImage
+  }
+}
+</script>
+
+<style>
+  .glass {
+    backdrop-filter: blur(3px);
+    background-color: rgba(255, 255, 255, 0.1);
+    border: 1px solid rgba(64, 224, 208, 0.2);
+    box-shadow: 0 4px 1px 1px transparent,
+      0 4px 20px 4px rgba(64, 224, 208, 0.2);
+    -webkit-backdrop-filter: blur(3px);
+  }
+
+  .board {
+    border-radius: 16px;
+    display: flex;
+    flex-wrap: wrap;
+    height: 70vh;
+    width: 50vw;
+  }
+
+  section {
+    display: flex;
+  }
+
+  .area-1 {
+    align-items: center;
+    height: 50%;
+    justify-content: center;
+    width: 30%;
+  }
+
+  .area-2 {
+    align-items: center;
+    height: 50%;
+    justify-content: center;
+    width: 70%;
+  }
+
+  .area-3 {
+    height: 50%;
+    width: 100%;
+  }
+</style>

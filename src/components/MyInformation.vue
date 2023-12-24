536bd07b04cc526cb851710807d4ccc84ac31823
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-12-24 15:19:57 -0300

    Atualizado o componente MyInformation

--- src/components/MyInformation.vue
+++ src/components/MyInformation.vue
@@ -1,23 +1,56 @@
 <template>
-  <InfoTable />
+  <p class="info">
+    Olá, meu nome é Isaac Oliveira e sou um Dev Front-end Junior.
+  </p>
+  <p class="info">
+    Nasci em 27 de Dezembro de 1991, sou uma pessoa motivada pelo estudo e pela busca por diversos tipos de desafios. Estou constantemente buscando oportunidades para aprender e me aprimorar!
+  </p>
+  <p class="info">
+    Neste projeto eu tenho como intuito apenas praticar o básico do Vue, como sua componentização e estruturação basica, além de renovar um antigo projeto feito em HTML e CSS puro.
+  </p>
+  <ul class="list">
+    <span>Algumas das minhas stacks são:</span>
+    <li>Git&Github</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>React</li>
+    <li>TypeScript</li>
+    <li>Vue</li>
+    <li>SQL</li>
+  </ul>
 </template>
 
 <script>
-import InfoTable from './InfoTable.vue'
-
 export default {
-  name: 'MyInformation',
-  components: {
-    InfoTable
-  },
-  props: {
-    msg: String
-  }
+  name: 'MyInformation'
 }
 </script>
 
 <style>
-a {
-  color: #42b983;
-}
+  .info, .list {
+    color: white;
+    font-size: larger;
+  }
+
+  .info {
+    margin: 1vh 0;
+    max-width: 90%;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: 85%;
+  }
+
+  .list span {
+    margin: 1vh 0;
+    width: 100%;
+  }
+
+  .list li {
+    width: 15%;
+  }
 </style>
